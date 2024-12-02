9b988bd071a423ab612c462036934ae28c6ace5d
Arshvohra <arshvohra4316@gmail.com>
2024-12-02 22:07:42 +0530

    Add Project Files

---
+++ controllers/reviews.js
@@ -0,0 +1,62 @@
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+
+
+module.exports.createReview = async(req, res) => { 
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+    req.flash("success", "New Review Created!");
+    res.redirect(`/listings/${listing._id}`);
+};
+
+module.exports.destroyReview = async(req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId }})
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "Review Deleted!");
+
+    res.redirect(`/listings/${id}`);
+};
+
+
+// const Listing = require("../models/listing");
+// const Review = require("../models/review");
+
+// module.exports.createReview = async (req, res) => { 
+//     try {
+//         let listing = await Listing.findById(req.params.id);
+//         let newReview = new Review(req.body.review);
+//         newReview.author = req.user._id;
+
+//         listing.reviews.push(newReview);
+
+//         await newReview.save();
+//         await listing.save();
+//         req.flash("success", "New Review Created!");
+//         res.redirect(`/listing/${listing._id}`);
+//     } catch (err) {
+//         req.flash("error", "Something went wrong while creating the review.");
+//         res.redirect("/listings");
+//     }
+// };
+
+// module.exports.destroyReview = async (req, res) => {
+//     let { id, reviewId } = req.params;
+
+//     try {
+//         await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+//         await Review.findByIdAndDelete(reviewId);
+//         req.flash("success", "Review Deleted!");
+//         res.redirect(`/listing/${id}`);
+//     } catch (err) {
+//         req.flash("error", "Something went wrong while deleting the review.");
+//         res.redirect("/listings");
+//     }
+// };
